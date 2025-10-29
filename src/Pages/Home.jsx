74314bcac0659a7ff077e1f82444df54f38a4f3f
rbriday <ridaya100@gmail.com>
2025-10-29 21:53:53 +0600

    fixed load and add useEffect hooks

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,27 +1,57 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Link, useNavigate } from "react-router";
 
 const Home = () => {
-    const auth = getAuth();
-    
-    const [verify, setVerify] = useState(false);
+  const auth = getAuth();
+  const navigate = useNavigate();
 
-    const data = useSelector(state => (state.userInfo.value))
+  const [verify, setVerify] = useState(false);
+  const [load, setLoad] = useState(true);
 
-onAuthStateChanged(auth, (user) => {
-  if (user.emailVerified) {
-    setVerify(true);
-  } 
-});
+  const data = useSelector((state) => state.userInfo.value);
 
-    return (
-        <div>
-            {
-                verify ? <p>home</p> : <p>Please verify your email</p>
-            }
+  useEffect(()=>{
+    if(!data){
+        navigate("/login")
+    }
+  })
+
+  onAuthStateChanged(auth, (user) => {
+    if (user.emailVerified) {
+      setVerify(true);
+    }
+    setLoad(false);
+  });
+
+  if(load){
+    return null;
+  }
+
+  return (
+    <div>
+      {verify ? (
+        <p>home</p>
+      ) : (
+        <div className="bg-primary w-full h-screen flex justify-center items-center">
+          <div className="bg-pink-200 w-[600px] rounded-xl text-center pt-[20px] pb-[30px] ">
+            <p className="font-primary font-bold text-[30px] text-primary tracking-[3px] mb-[20px]">
+              Please verify your email
+            </p>
+            <Link
+              to="/login"
+              type="button"
+              className="relative border-2 border-primary bg-primary px-[16px] py-[7px] rounded-[8px] font-primary font-semibold text-white tracking-[2px] z-[1] cursor-pointer"
+            >
+              Go Back
+              <span className="absolute top-1/2 left-1/2 translate-[-50%] bg-red-500 blur-lg w-[30px] h-[30px] z-[-1]"></span>
+            </Link>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default Home;
